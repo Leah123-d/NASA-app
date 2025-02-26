dfd2873dc2e5f32caa8ddee16d72e92cdee4b488
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-02-26 00:10:43 -0800

    Updating routes

--- app-backend/src/index.js
+++ app-backend/src/index.js
@@ -1,9 +1,18 @@
 import express from 'express'
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import apiRoutes from './routes/apiRoutes.js';
+
+dotenv.config();
+
+app.use(bodyParser.json()) //Middleware to parse JSON
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) => {
+// app.use('/',) will be connected to routes 
+
+app.get('/', (req,res) => { //a connection to the homepage
   res.send("Hello World")
 })
 
