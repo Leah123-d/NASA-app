e0a2ce3ac8f5d71a986246bc908dce27805ef222
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-02-27 09:23:07 -0800

    reformat

--- app-front-end/src/App.jsx
+++ app-front-end/src/App.jsx
@@ -4,27 +4,9 @@ import Navbar2 from './components/Navbar2.jsx'
 import Card from './components/Card.jsx'
 import Footer from './components/Footer.jsx'
 import { useState } from 'react';
-import dotenv from 'dotenv';
-// const { authToken } = process.env;
 
 function App() {
-  
   const [data, setData] = useState('');
-
-  async function fetchData() {
-    try{
-      const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${authToken}`) //get the URL and pass it to the fetch function as a string
-      //once the fetch resolves we have to check if the response is okay
-      if(!response.ok){
-        throw new Error("Media not found");
-      }
-      const data = await response.json();//the response is converted to json and this returns a promise 
-      console.log(data);
-    }
-    catch(error){
-      console.log(error);
-    }
-  }
 
   const connectToBackend = () => {
     fetch('/api')
@@ -49,9 +31,8 @@ function App() {
                     Send Request to Backend
                     </button>
                     <p>{data}</p>
-
-                    <input type="text" id="media-title" placeholder="enter title"/>
-                    <button onClick={fetchData}>Fetch Data</button>
+                    
+                    {/* button is working, but now i have to see how I can get a response or if my env variables are reading correctly */}
                 </div>
 
       <Navbar2 />
