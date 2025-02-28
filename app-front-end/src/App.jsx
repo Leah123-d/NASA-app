dd7d7b4a1e1e3a141e58a6e03dbfb5a49191570e
Winnie Kelley <w642kelley@gmail.com>
2025-02-28 11:57:52 -0600

    Adjusted some formatting on lines 45 - 53

--- app-front-end/src/App.jsx
+++ app-front-end/src/App.jsx
@@ -43,14 +43,14 @@ function App() {
       {/* Render the newly fetched data inside data */}
 
       <div className="App">
-                    <h1>React Website with an Express backend</h1>
-                    <button  onClick={NASADatafromBE}>
-                    Send Request to Backend
-                    </button>
-                    <p>{info}</p>
-                    
-                    {/* button is working, but now i have to see how I can get a response or if my env variables are reading correctly */}
-                </div>
+        <h1>React Website with an Express backend</h1>
+        <button  onClick={NASADatafromBE}>
+        Send Request to Backend
+        </button>
+        <p>{info}</p>
+        
+        {/* button is working, but now i have to see how I can get a response or if my env variables are reading correctly */}
+      </div>
 
       <Navbar2 />
       <Card />
